9677c6fbe0c63550f7597cd17b5b73a983aae016
tuanminh160302 <canhtuan09@gmail.com>
2022-06-07 21:03:32 -0400

    styling touch up

--- src/components/Header.component.jsx
+++ src/components/Header.component.jsx
@@ -32,15 +32,15 @@ const Header = () => {
             {
                 user ?
                     <div className="w-1/6 bg-bg_navy h-screen absolute top-0 left-0 rounded-r-lg">
-                        {/* <button className="bg-bg_light px-6 py-4 rounded-lg"><a className="header-actions text-black" onClick={() => handleSignout()}>Logout</a></button> */}
+                        <button className="bg-bg_light px-6 py-4 rounded-lg" onClick={() => handleSignout()}><a className="header-actions text-black">Logout</a></button>
                     </div> : null
             }
             <a className={`header-actions text-black ${user ? 'ml-[18%]' : null}`} href="/">Connect</a>
             {
                 !user ?
-                    <button className="bg-bg_navy px-6 py-4 rounded-lg"><a className="header-actions" href="/login">Login</a></button> : 
+                    <a href="/login"><button className="bg-bg_navy px-6 py-4 rounded-lg header-actions">Login</button></a> :
                     <div className="rounded-full box-border h-14 w-14 relative">
-                        <img className="rounded-full h-full w-full cursor-pointer" src={user.photoURL} alt="avatar" onClick={() => {handleToggleModal()}}/>
+                        <img className="rounded-full h-full w-full cursor-pointer" src={user.photoURL} alt="avatar" onClick={() => { handleToggleModal() }} />
                         {/* {showProfileModal && <div ref={profileModal} className="mt-3 h-80 w-64 bg-bg_navy shadow-xl shadow-cyan-900/40 absolute right-0 rounded-xl"></div>} */}
                         <div ref={profileModal} className="mt-3 h-80 w-64 bg-bg_navy shadow-sm shadow-cyan-900/30 absolute right-0 rounded-xl origin-top scale-y-0"></div>
                     </div>
