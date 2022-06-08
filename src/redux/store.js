a5019ba7bebfc001ca84944913c9cda8e7847b50
tuanminh160302 <canhtuan09@gmail.com>
2022-06-08 01:29:32 -0400

    update redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import logger from "redux-logger";
 import preloaderReducer from "./preloaderSlice";
 
 const store = configureStore({
-    middleware: [logger],
     reducer: {
         preloader: preloaderReducer,
     }
